86547177485d35ba49fce2edd55f957779f0e216
Julie Marin <julie.sylvana.marin@gmail.com>
2018-10-15 12:02:30 -0400

    Override arguments passed to gateway with profiles

--- core/app/models/spree/payment/processing.rb
+++ core/app/models/spree/payment/processing.rb
@@ -48,7 +48,7 @@ module Spree
           if payment_method.payment_profiles_supported?
             # Gateways supporting payment profiles will need access to credit card object because this stores the payment profile information
             # so supply the authorization itself as well as the credit card, rather than just the authorization code
-            response = payment_method.void(self.response_code, source, gateway_options)
+            response = payment_method.void(self.transaction_id, gateway_options)
           else
             # Standard ActiveMerchant void usage
             response = payment_method.void(self.response_code, gateway_options)
